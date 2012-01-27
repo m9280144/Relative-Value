f657a89b741779dbdece278df7a1231ce7289a13
Matt Fg <matthias9280144@gmail.com>
2012-01-27 16:24:06 +0100

    initial add

---
+++ RV_logic/src/main/java/de/matthias/rv/logic/domain/Bond.java
@@ -0,0 +1,20 @@
+package de.matthias.rv.logic.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matthias
+ * Date: 27.01.12
+ * Time: 15:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bond {
+    private int ISIN;
+
+    public int getISIN() {
+        return ISIN;
+    }
+
+    public void setISIN(int ISIN) {
+        this.ISIN = ISIN;
+    }
+}
